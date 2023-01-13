a1d36a01277a4941df48c722865f3a8d6d4351d6
Darien Gillespie <dsgillespie02@yahoo.com>
2023-01-13 16:47:03 -0500

    Initial Commit

---
+++ src/main/java/org/infernalstudios/jsonentitymodelsextras/JSONEntityModelsExtras.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2023 Infernal Studios
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.infernalstudios.jsonentitymodelsextras;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(JSONEntityModelsExtras.MOD_ID)
+public class JSONEntityModelsExtras {
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "jsonentitymodelsextras";
+
+    public JSONEntityModelsExtras() {
+        final ModLoadingContext modLoadingContext = ModLoadingContext.get();
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+    }
+}
